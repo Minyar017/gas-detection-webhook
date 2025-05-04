f5e0b8a5de5e5e1ab44ceb25b71d8b24905f14ca
salmine1 <153761259+salmine1@users.noreply.github.com>
2025-05-04 21:45:01 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -56,8 +56,7 @@ def process_command():
     print(f"Processing intent: {intent}")
 
     # Handle Default Welcome Intent
-    if intent == 'Default_Welcome_Intent':
-        response = "Bonjour ! Je suis ici pour vous aider à surveiller les niveaux de CO. Posez-moi une question comme 'Quel est le niveau de CO ?' ou 'Est-ce dangereux ?'."
+    if intent == 'Default Welcome Intent':
         print(f"Returning response: {response}")
         return jsonify({
             'fulfillmentText': response
