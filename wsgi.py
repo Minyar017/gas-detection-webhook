8348d0d2e42e21f2b82ce6159957ee5d621e0e26
salmine1 <153761259+salmine1@users.noreply.github.com>
2025-05-05 18:19:05 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,16 +5,17 @@ import json
 
 # Initialize Firebase once
 try:
-    firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
-    if firebase_credentials:
-        cred_dict = json.loads(firebase_credentials)
-        cred = credentials.Certificate(cred_dict)
-    else:
-        cred = credentials.Certificate('serviceAccountKey.json')
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://projet-fin-d-etude-4632f-default-rtdb.firebaseio.com/'
-    })
-    print("✅ Firebase initialized")
+    if not firebase_admin.get_app():  # Check if Firebase is already initialized
+        firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
+        if firebase_credentials:
+            cred_dict = json.loads(firebase_credentials)
+            cred = credentials.Certificate(cred_dict)
+        else:
+            cred = credentials.Certificate('serviceAccountKey.json')
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': 'https://projet-fin-d-etude-4632f-default-rtdb.firebaseio.com/'
+        })
+        print("✅ Firebase initialized")
 except Exception as e:
     print(f"❌ Error initializing Firebase: {e}")
 
